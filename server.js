7144e9fee1ed3d139d18642d4d2c46ec922143fc
borchardt <jackson.borchardt@gmail.com>
2020-07-24 23:05:46 -0700

    put finally working (on http)

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const Minio = require('minio');
 
 const minioClient = new Minio.Client({
   endPoint: process.env.HOST_NAME,
-  port: 9000,
   useSSL: false,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
